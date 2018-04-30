5285c22b442c6f3c41dd251cda5ac6f471f34443
Terrance Mount <thorelore@gmail.com>
2018-04-29 20:18:15 -0800

    Added mock server with post and get to the forms too.

--- Node/node_server/server.js
+++ Node/node_server/server.js
@@ -1,37 +1,75 @@
-var http = require('http');
-var formidable = require("formidable");
+/****************************************************************
+ * This is simple server to handel a mock form post and get an
+ * array of languages for a form select.  Not for production.
+ * 
+ * Use > node server.js 
+ * to start the server.
+ * 
+ * It listens on 3100.
+ * 
+ * 
+ ***************************************************************/
+var http = require('http'); //built in http server in node.js.  Express is not needed here.
+var formidable = require("formidable"); //allow easy use of forms
 var util = require('util');
 
 var server = http.createServer(function (req, res) {
+    //setup cross-site headers to allow form on 3000 to talk to the server on 3100
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
+    //this will handle a mock post from the form
     if (req.method.toLowerCase() == 'post') {
         processForm(req, res);
+        return;
+    }
+
+    //this will handel a mock get for the languages array for the form
+    if(req.method.toLocaleLowerCase() == 'get'){
+        var data = {
+            data: {
+                languages: [
+                    'English',
+                    'Spanish',
+                    'German',
+                    'French',
+                    'Other'
+                ]
+            }
+        };
+
+        //create stringified version of the array above.
+        var responseData = JSON.stringify(data);
+        
+        //send it back to the requester for the get... the angular form.  
+        res.end(responseData);
+        console.log('get: ', responseData);
         return;
     }
 
     res.end();
 });
 
-
+//this is what poccess the form
 function processForm(req, res) {
+    //this is what will parse the form from the body of the http post. 
     var form = new formidable.IncomingForm();
 
+    //this is waht sets up fields array for us.
     form.parse(req, function (err, fields) {
 
         res.writeHead(200, {
             'content-type': 'text/plain'
         });
         
-        res.end(util.inspect({
-            fields: fields
-        }));
+        fields.id = 'abc123'; //add the id back to the field object.
+
+        var data = JSON.stringify({fields: fields});
+
+        res.end(data);
 
         console.log('posted fields:\n');
-        console.log(util.inspect({
-            fields: fields
-        }));
+        console.log(data);
     });
 }
 
--- angular/angular2-forms/src/app/home/home.component.ts
+++ angular/angular2-forms/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import { Employee } from '../models/emplyee.model';
+import { FormPoster } from '../services/form-poster.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -7,9 +9,19 @@ import { Employee } from '../models/emplyee.model';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  languages = ["English", "Spanish", "Foo", "Other"];
+  languages = [];
   model = new Employee('', '', true, 'w2', 'default');
   hasPrimaryLanguageError = false;
+  employeeId = 0;
+
+  constructor(private formPoster:FormPoster){
+    //immediatly request the mock server for the language array.
+    this.formPoster.getLanguages()
+      .subscribe( //this subsribe will make the observable will reach out to the server.
+        data => this.languages = data.languages,
+        err => console.log('get error: ', err)
+      );
+  }
 
   textToUpperCase(value: string){
     this.model.firstName = value.toUpperCase();
@@ -18,5 +30,24 @@ export class HomeComponent {
   validPrimaryLanguage(value: string){
       this.hasPrimaryLanguageError = value === 'default';
   }
+
+  
+  submitForm(form: NgForm){
+    //note: the submit button will be disabled if the form is invalid.  
+    
+    //call the validate primary lang first
+    this.validPrimaryLanguage(this.model.primaryLanguage);
+    
+    //check if the is a language error.
+    if(this.hasPrimaryLanguageError)
+      return; //cancle proccessing
+    
+    this.formPoster.postEmployeeForm(this.model)
+      .subscribe(
+        data => console.log('success: ', data), //you can get the id out of data
+        err => console.log('error: ', err)
+      )
+  }
+
 
 }
