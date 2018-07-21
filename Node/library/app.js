247fe9a7a38974df342c534bc61215d43f6742e8
Terrance Mount <thorelore@gmail.com>
2018-07-20 17:34:07 -0800

    Node Tutorial

--- Node/library/app.js
+++ Node/library/app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000; // pull in the enviromental varable from nodmon config
 
 app.use(morgan('tiny'));
 
@@ -14,10 +15,10 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(3000, ()=>{
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(3000, () => {
+  debug(`listening at port ${chalk.green(port)}`);
 });
